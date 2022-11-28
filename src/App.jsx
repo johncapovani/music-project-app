23e3b366c71708a1a8f0f426ea21c7d0a66a5f97
John Capovani <john@capovani.com>
2022-11-27 19:37:32 -0500

    React-router-dom added to the application

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Fragment } from 'react';
+//Router Import for react-router-dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+//Import components
+import SearchBar from './components/SearchBar'
+import Gallery from './components/Gallery'
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
+
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          return setData(resData.results)
+        } else {
+          return setMessage('Not Found.')
+        }
+      }
+      fetchData()
+    }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
+  return (
+    <div className='App'>
+    
+        {message}
+        <Router>
+          <Routes>
+            <Route path="/" element={
+              //Fragment allows us to pass multiple elements in one route
+              <Fragment>
+                <SearchBar handleSearch={handleSearch} />
+                <Gallery data={data} />
+              </Fragment>
+            } />
+            <Route path="/album/:id" element={<AlbumView />} />
+            <Route path="/artist/:id" element={<ArtistView />} />
+          </Routes>
+        </Router>
+      </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
